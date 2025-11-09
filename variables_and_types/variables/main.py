dfa651e7b00e9db0aee99bd4b32695d111d0c5d6
rumihoney <>
2025-11-09 19:39:28 +0000

    Variables

---
+++ variables_and_types/variables/main.py
@@ -0,0 +1,11 @@
+item_name = "Banana" #string
+item_price = 3.50 #float
+item_stock_quantity = 100 #integer
+in_stock = True #boolean
+
+
+# Testing
+print("Item : ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
